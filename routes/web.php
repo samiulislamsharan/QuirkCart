274d4c71d3ea8393cf13a456e8794fd13ea83628
Samiul Islam <samiulislamsharan@gmail.com>
2024-08-26 21:35:22 +0600

    refactor(routes): :recycle: refactor namespaces to use the new controller directory

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\AdminAuthController;
-use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
