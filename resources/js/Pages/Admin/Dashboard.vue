3cda6a8c2b648166ed15c53b57c8d5ef10484cf2
Samiul Islam <samiulislamsharan@gmail.com>
2024-08-26 21:45:13 +0600

    feat(views): :sparkles: add new vue file to show admin dashboard

---
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -0,0 +1,37 @@
+<template>
+    <AdminLayout>
+        <main class="h-auto p-4 pt-20 md:ml-64">
+            <div class="grid grid-cols-1 gap-4 mb-4 sm:grid-cols-2 lg:grid-cols-4">
+                <div class="h-32 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-64"></div>
+                <div class="h-32 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-64"></div>
+                <div class="h-32 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-64"></div>
+                <div class="h-32 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-64"></div>
+            </div>
+            <div class="mb-4 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 h-96"></div>
+            <div class="grid grid-cols-2 gap-4 mb-4">
+                <div class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"></div>
+                <div class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"></div>
+                <div class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"></div>
+                <div class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"></div>
+            </div>
+            <div class="mb-4 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 h-96"></div>
+            <div class="grid grid-cols-2 gap-4">
+                <div class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"></div>
+                <div class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"></div>
+                <div class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"></div>
+                <div class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"></div>
+            </div>
+        </main>
+    </AdminLayout>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+import AdminLayout from './Components/AdminLayout.vue';
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+</script>
