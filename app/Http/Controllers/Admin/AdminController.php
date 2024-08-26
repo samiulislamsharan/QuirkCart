8a203dbc17c80b2fe653dced91298ba87645ca92
Samiul Islam <samiulislamsharan@gmail.com>
2024-08-26 21:32:42 +0600

    refactor(controllers): :recycle: move AdminController and AdminAuthController to Admin directory
    This commit moves two controller classes to new Admin directry to maintain clean code and organization.

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render(
+            'Admin/Dashboard',
+            [
+                'admin' => 'admin'
+            ]
+        );
+    }
+}
