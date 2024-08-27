5b34f2ac9555409d33b02f66f239594e622cc549
Samiul Islam <samiulislamsharan@gmail.com>
2024-08-28 02:13:59 +0600

    refactor(views): :recycle: update AdminLayout.vue to include a main content section for better page structure

--- resources/js/Pages/Admin/Components/AdminLayout.vue
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -6,7 +6,10 @@
         <!-- Sidebar -->
         <Sidebar />
 
-        <slot />
+        <!-- Main Content -->
+        <main class="h-auto p-4 pt-20 md:ml-64">
+            <slot />
+        </main>
     </div>
 </template>
 
