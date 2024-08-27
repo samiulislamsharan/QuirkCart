a18392b1a72c5fbe257a745143de85cbc9e57999
Samiul Islam <samiulislamsharan@gmail.com>
2024-08-28 01:53:05 +0600

    feat(models): :sparkles: add HasSlug trait to Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,35 @@
 
 namespace App\Models;
 
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    use HasSlug;
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'published',
+        'in_stock',
+        'price',
+        'created_by',
+        'updated_by',
+        'deleted_by',
+    ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
