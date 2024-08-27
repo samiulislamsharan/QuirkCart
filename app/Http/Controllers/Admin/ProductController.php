db529cdbb25a47022f618f488e8c6c9cc64ad739
Samiul Islam <samiulislamsharan@gmail.com>
2024-08-28 02:17:23 +0600

    feat(controller): :sparkles: add ProductController for managing products in the admin panel

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::get();
+
+        return Inertia::render('Admin/Products/Index', [
+            'products' => $products,
+        ]);
+    }
+}
