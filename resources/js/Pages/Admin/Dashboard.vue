f55219e4c015a01cc0965e7cca5b12965f3e519b
Samiul Islam <samiulislamsharan@gmail.com>
2024-08-28 02:12:33 +0600

    fix(views): :bug: update Admin Dashboard layout to fix responsive design issue

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
     <AdminLayout>
-        <main class="h-auto p-4 pt-20 md:ml-64">
+        <div>
             <div class="grid grid-cols-1 gap-4 mb-4 sm:grid-cols-2 lg:grid-cols-4">
                 <div class="h-32 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-64"></div>
                 <div class="h-32 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-64"></div>
@@ -21,7 +21,7 @@
                 <div class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"></div>
                 <div class="h-48 border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-600 md:h-72"></div>
             </div>
-        </main>
+        </div>
     </AdminLayout>
 </template>
 
