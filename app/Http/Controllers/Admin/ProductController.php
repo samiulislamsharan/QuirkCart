561637a497307287bbab107d0944cf77a39c2125
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-08 00:31:23 +0600

    feat(controllers): :sparkles: add Brand and Category models to ProductController index method

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -12,9 +14,13 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::get();
+        $categories = Category::get();
+        $brands = Brand::get();
 
         return Inertia::render('Admin/Products/Index', [
             'products' => $products,
+            'categories' => $categories,
+            'brands' => $brands,
         ]);
     }
 }
