c18800953e536a399a28f7d40a39398e9cd9e8cd
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-08 00:26:48 +0600

    feat(models): :sparkles: add fillable and SlugOptions to Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    use HasSlug;
     use HasFactory;
-}
+
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
