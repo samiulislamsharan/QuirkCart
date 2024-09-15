a4ab180c5fbc2f7643de884df220c749c03269dc
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-16 01:58:13 +0600

    feat(controllers): :sparkles: eager load category, brand and product_images relationship in product model

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -15,15 +15,22 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::get();
+        $products = Product::with(
+            'category',
+            'brand',
+            'product_images'
+        )->get();
         $categories = Category::get();
         $brands = Brand::get();
 
-        return Inertia::render('Admin/Products/Index', [
-            'products' => $products,
-            'categories' => $categories,
-            'brands' => $brands,
-        ]);
+        return Inertia::render(
+            'Admin/Products/Index',
+            [
+                'products' => $products,
+                'categories' => $categories,
+                'brands' => $brands,
+            ]
+        );
     }
 
     public function store(Request $request)
