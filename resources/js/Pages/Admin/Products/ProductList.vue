9cb2e54ff24dd24735eec42286006908e7b44c65
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-15 13:54:51 +0600

    feat(components): render product, category and brand list in ProductList.vue

--- resources/js/Pages/Admin/Products/ProductList.vue
+++ resources/js/Pages/Admin/Products/ProductList.vue
@@ -216,8 +216,9 @@
                         <tbody>
                             <tr v-for="product in products" :key="product.id" class="border-b dark:border-gray-700">
                                 <th scope="row"
-                                    class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{
-                                        product.title }}</th>
+                                    class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ product.title }}
+                                </th>
                                 <td class="px-4 py-3">{{ product.category_id }}</td>
                                 <td class="px-4 py-3">{{ product.brand_id }}</td>
                                 <td class="px-4 py-3">{{ product.quantity }}</td>
@@ -225,8 +226,7 @@
                                 <td class="px-4 py-3">{{ product.published }}</td>
                                 <td class="px-4 py-3">{{ product.price }}</td>
                                 <td class="flex items-center justify-end px-4 py-3">
-                                    <button id="apple-imac-27-dropdown-button"
-                                        data-dropdown-toggle="apple-imac-27-dropdown"
+                                    <button :id="`${product.id}-button`" :data-dropdown-toggle="`${product.id}`"
                                         class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
                                         type="button">
                                         <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20"
@@ -235,17 +235,17 @@
                                                 d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                         </svg>
                                     </button>
-                                    <div id="apple-imac-27-dropdown"
+                                    <div :id="`${product.id}`"
                                         class="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                                         <ul class="py-1 text-sm text-gray-700 dark:text-gray-200"
-                                            aria-labelledby="apple-imac-27-dropdown-button">
+                                            aria-labelledby="`${product.id}-button`">
                                             <li>
                                                 <a href="#"
                                                     class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Show</a>
                                             </li>
                                             <li>
-                                                <a href="#"
-                                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
+                                                <button @click="openEditModal(product)"
+                                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</button>
                                             </li>
                                         </ul>
                                         <div class="py-1">
@@ -326,6 +326,8 @@ import { usePage } from '@inertiajs/vue3';
 import { ref } from 'vue';
 
 const products = usePage().props.products;
+const categories = usePage().props.categories;
+const brands = usePage().props.brands;
 
 const dialogVisible = ref(false)
 const editMode = ref(false)
