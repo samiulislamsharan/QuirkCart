825b0c26ce080c940976258a8d89edb9ec750085
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-16 02:06:17 +0600

    refactor(components): :bulb: add necessary comments for better readability and clean code

--- resources/js/Pages/Admin/Products/ProductList.vue
+++ resources/js/Pages/Admin/Products/ProductList.vue
@@ -76,6 +76,7 @@
                     </el-upload>
                 </div>
 
+                <!-- list of images for selected product start -->
                 <div class="flex mb-8 flex-nowrap">
                     <div v-for="(product_image, index) in product_images" :key="product_image.id"
                         class="relative w-32 h-32 ">
@@ -93,7 +94,8 @@
                         </span>
                     </div>
                 </div>
-                <!-- product images start end-->
+                <!-- list of images for selected product end -->
+                <!-- product images end-->
 
                 <button type="submit"
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
