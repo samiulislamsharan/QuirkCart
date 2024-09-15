9ca84361d05f5d3fb27fed4ef607f5c58f9a5c7b
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-15 12:32:39 +0600

    refactor(components): :recycle: update product to add modal for adding products in ProductList.vue

--- resources/js/Pages/Admin/Products/ProductList.vue
+++ resources/js/Pages/Admin/Products/ProductList.vue
@@ -1,5 +1,99 @@
 <template>
     <section class="p-3 bg-gray-50 dark:bg-gray-900 sm:p-5">
+        <!-- Modal start here -->
+        <el-dialog v-model="dialogVisible" :title="editMode ? 'Edit Product' : 'Add Product'" width="650"
+            :before-close="handleClose">
+            <form class="mx-auto" @submit.prevent="AddProduct()">
+                <div class="relative z-0 w-full mb-5 group">
+                    <input v-model="title" type="text" name="floating_title" id="floating_title"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" " required />
+                    <label for="floating_title"
+                        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Product
+                        Title</label>
+                </div>
+                <div class="relative z-0 w-full mb-5 group">
+                    <input v-model="price" type="number" min="0" name="floating_price" id="floating_price"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" " required />
+                    <label for="floating_price"
+                        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Price</label>
+                </div>
+                <div class="relative z-0 w-full mb-5 group">
+                    <input v-model="quantity" type="number" min="0" step="1" name="floating_quantity"
+                        id="floating_quantity"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                        placeholder=" " required />
+                    <label for="floating_quantity"
+                        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Quantity</label>
+                </div>
+
+                <div class="grid md:grid-cols-2 md:gap-6">
+                    <div class="relative z-0 w-full mb-5 group">
+                        <!-- category dropdown start -->
+                        <label for="categories"
+                            class="block mb-2 text-sm font-medium text-gray-500 peer-focus:font-medium dark:text-gray-400peer-focus:text-blue-600">Select
+                            an option</label>
+                        <select id="categories" v-model="category_id"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option v-for="category in categories" :key="category.id" :value="category.id">{{
+                                category.name }}</option>
+                        </select>
+                        <!-- category dropdown end -->
+                    </div>
+                    <div class="relative z-0 w-full mb-5 group">
+                        <!-- brand dropdown start -->
+                        <label for="brands"
+                            class="block mb-2 text-sm font-medium text-gray-500 peer-focus:font-medium dark:text-gray-400peer-focus:text-blue-600">Select
+                            an
+                            option</label>
+                        <select id="brands" v-model="brand_id"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option v-for="brand in brands" :key="brand.id" :value="brand.id">{{ brand.name }}</option>
+                        </select>
+                        <!-- brand dropdown end -->
+                    </div>
+                </div>
+
+                <div class="relative z-0 w-full mb-5 group">
+                    <label for="message"
+                        class="block mb-2 text-sm font-medium text-gray-500 peer-focus:font-medium dark:text-gray-400peer-focus:text-blue-600">Description</label>
+                    <textarea v-model="description" id="message" rows="4"
+                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Write product description here..."></textarea>
+                </div>
+                <!-- product images start -->
+                <div class="relative z-0 w-full mb-5 group">
+                    <label for="message"
+                        class="block mb-2 text-sm font-medium text-gray-500 peer-focus:font-medium dark:text-gray-400peer-focus:text-blue-600">Upload
+                        Image(s)</label>
+                    <el-upload v-model:file-list="productImages" list-type="picture-card" multiple
+                        :on-preview="handlePictureCardPreview" :on-remove="handleRemove" :on-change="handleFileChange"
+                        :auto-upload="false">
+                        <el-icon>
+                            <Plus />
+                        </el-icon>
+                    </el-upload>
+                </div>
+
+                <!-- list of images for selected product -->
+                <div class="flex mb-8 flex-nowrap ">
+                    <div v-for="(pimage, index) in product_images" :key="pimage.id" class="relative w-32 h-32 ">
+                        <img class="w-24 h-20 rounded" :src="`/${pimage.image}`" alt="">
+                        <span
+                            class="absolute top-0 right-8 transform -translate-y-1/2 w-3.5 h-3.5 bg-red-400 border-2 border-white dark:border-gray-800 rounded-full">
+                            <span @click="deleteImage(pimage, index)"
+                                class="absolute text-xs font-bold text-white transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">x</span>
+                        </span>
+                    </div>
+                </div>
+                <!-- product images start end-->
+
+                <button type="submit"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            </form>
+        </el-dialog>
+        <!-- Modal end here -->
         <div class="max-w-screen-xl px-4 mx-auto lg:px-12">
             <!-- Start coding here -->
             <div class="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
