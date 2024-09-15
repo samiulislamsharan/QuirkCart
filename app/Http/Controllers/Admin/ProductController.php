83635394513e2652c857ec546790f8967ff3b89f
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-16 02:01:04 +0600

    feat(controllers): :sparkle: add update method for updating product details in ProductController

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -66,4 +66,39 @@ class ProductController extends Controller
 
         return redirect()->route('admin.products.index')->with('success', 'Product created successfully');
     }
-}
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $product->title = $request->title;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->description = $request->description;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+
+        /**
+         * check if product images have been uploaded
+         * if yes, save them to the database
+         */
+        if ($request->hasFile('product_images')) {
+            // dd($request->file('product_images'));
+
+            $productImages = $request->file('product_images');
+
+            foreach ($productImages as $productImage) {
+                $uniqueName = time() . '-' . Str::random(10) . '.' . $productImage->getClientOriginalName();
+                $productImage->move('product_images', $uniqueName);
+
+                ProductImage::create([
+                    'product_id' => $product->id,
+                    'image' => 'product_images/' . $uniqueName,
+                ]);
+            }
+        }
+
+        $product->update();
+
+        return redirect()->back()->with('success', 'Product updated successfully');
+    }
