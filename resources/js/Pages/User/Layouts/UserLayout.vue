0bdbe2f135b0b6b4fb8234a690011c8eeaeb1a27
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-16 18:28:09 +0600

    feat(components): :sparkles: add UserLayout component with Navbar, Hero, and Footer

---
+++ resources/js/Pages/User/Layouts/UserLayout.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- navbar start -->
+    <Navbar />
+    <!-- navbar end -->
+
+    <!-- main content start -->
+
+    <!-- hero section start -->
+    <Hero />
+    <!-- hero section end -->
+
+    <slot />
+    <!-- main content end -->
+
+    <!-- footer start -->
+    <Footer />
+    <!-- footer end -->
+
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+import Hero from '../Components/Hero.vue';
+import Navbar from '../Components/Navbar.vue';
+import Footer from '../Components/Footer.vue';
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+</script>
