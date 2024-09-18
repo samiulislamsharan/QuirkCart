d6c09968ccbcf05a6aef95311607dc8ae2570571
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-18 11:56:28 +0600

    feat(controller): :sparkles: add CartController with view, store, update, and destroy methods

---
+++ app/Http/Controllers/User/CartController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function view()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function update()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
+}
