fb42b7a80aef98644dcd6fc3022263d4bae35365
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-20 23:15:27 +0600

    feat(helpers): :sparkles: add getCartItems method to Cart class

--- app/Helpers/Cart.php
+++ app/Helpers/Cart.php
@@ -13,4 +13,18 @@ class Cart
             return CartItem::where('user_id', $user->id)->sum('quantity');
         }
     }
+
+    public static function getCartItems()
+    {
+        if ($user = auth()->user()) {
+            return CartItem::where('user_id', $user->id)
+                ->get()
+                ->map(
+                    fn(CartItem $item) => [
+                        'product_id' => $item->product_id,
+                        'quantity' => $item->quantity
+                    ]
+                );
+        }
+    }
 }
