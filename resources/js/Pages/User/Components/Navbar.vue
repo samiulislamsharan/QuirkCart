f0882e4562a79ed63914b52b8a70717203b5037f
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-20 23:20:19 +0600

    refactor(components): :recycle: improve user dropdown visibility by defining logout route in the button

--- resources/js/Pages/User/Components/Navbar.vue
+++ resources/js/Pages/User/Components/Navbar.vue
@@ -68,9 +68,10 @@
                                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
                         </li>
                         <li>
-                            <a href="#"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
-                                out</a>
+                            <Link :href="route('logout')" method="post"
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                            Sign out
+                            </Link>
                         </li>
                     </ul>
                 </div>
