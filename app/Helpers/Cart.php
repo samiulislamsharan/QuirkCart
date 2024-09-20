3a9f894e14e418b2e6fe04279b63b224b6bdd02d
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-20 23:14:59 +0600

    feat(helpers): :sparkles: add getCount method to Cart class

--- app/Helpers/Cart.php
+++ app/Helpers/Cart.php
@@ -7,4 +7,10 @@ use Illuminate\Support\Facades\Cookie;
 
 class Cart
 {
+    public static function getCount()
+    {
+        if ($user = auth()->user()) {
+            return CartItem::where('user_id', $user->id)->sum('quantity');
+        }
+    }
 }
