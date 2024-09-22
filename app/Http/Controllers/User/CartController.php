ec8e49fc737a6b296953bc99752853d06b075c4f
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-22 16:59:19 +0600

    refactor(controllers): :recycle: update store method in CartController.php
    - Add use statements for Cart helper, CartItem model, and Product model
    - Modify store() method to handle adding products to cart for authenticated and non-authenticated users
    - Update logic to increment quantity for existing cart items or create new cart items
    - Update logic to handle adding products to cookie cart for non-authenticated users
    - Return redirect with success message after adding product to cart

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Helper\Cart;
 use App\Http\Controllers\Controller;
+use App\Models\CartItem;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
@@ -12,12 +15,56 @@ class CartController extends Controller
         //
     }
 
-    public function store()
+    public function store(Request $request, Product $product)
     {
-        //
-    }
+        $quantity = $request->post('quantity', 1);
+        $user = $request->user();
 
-    public function update()
+        if ($user) {
+            $cartItem = CartItem::where(
+                [
+                    'user_id' => $user->id,
+                    'product_id' => $product->id
+                ]
+            )->first();
+
+            if ($cartItem) {
+                $cartItem->increment('quantity');
+            } else {
+                CartItem::create(
+                    [
+                        'user_id' => $user->id,
+                        'product_id' => $product->id,
+                        'quantity' => $quantity
+                    ]
+                );
+            }
+        } else {
+            $cartItems = Cart::getCookieCartItems();
+            $productExists = false;
+
+            foreach ($cartItems as $cartItem) {
+                if ($cartItem['product_id'] === $product->id) {
+                    $cartItem['quantity'] += $quantity;
+                    $productExists = true;
+                    break;
+                }
+            }
+
+            if (!$productExists) {
+                $cartItems[] = [
+                    'user_id' => null,
+                    'product_id' => $product->id,
+                    'quantity' => $quantity,
+                    'price' => $product->price,
+                ];
+            }
+
+            Cart::setCookieCartItems($cartItems);
+        }
+
+        return redirect()->back()->with('success', 'Product added to cart successfully!');
+    }
     {
         //
     }
