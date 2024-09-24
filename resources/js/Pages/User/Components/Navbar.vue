54bf999eba37f1b5996416210ff5729d7319880a
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-25 00:47:50 +0600

    refactor(components): :recycle: add to cart button link in Navbar.vue

--- resources/js/Pages/User/Components/Navbar.vue
+++ resources/js/Pages/User/Components/Navbar.vue
@@ -56,8 +56,10 @@
                     </div>
                     <ul class="py-2" aria-labelledby="user-menu-button">
                         <li>
-                            <a href="#"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                            <Link :href="route('dashboard')"
+                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                            Dashboard
+                            </Link>
                         </li>
                         <li>
                             <a href="#"
