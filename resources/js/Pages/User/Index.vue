077fc0d8af2626ae9a7be7de8a3ba155b1d8e844
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-24 23:35:16 +0600

    feat(components): :sparkles: add view product hover button in Index.vue

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -16,10 +16,11 @@
                                 src="https://png.pngtree.com/png-vector/20221125/ourmid/pngtree-no-image-available-icon-flatvector-illustration-pic-design-profile-vector-png-image_40966566.jpg"
                                 class="object-contain object-center w-full h-full lg:h-full lg:w-full" />
 
-                            <!-- add to cart hover button start-->
+                            <!-- add to cart hover button start -->
                             <div
                                 class="absolute inset-0 flex items-center justify-center opacity-0 cursor-pointer group-hover:opacity-100">
-                                <div class="p-2 bg-white rounded-full" @click="addToCart(product)">
+                                <div class="p-2 bg-white rounded-full">
+                                    <a @click="addToCart(product)">
                                     <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
                                         viewBox="0 0 24 24">
@@ -27,9 +28,25 @@
                                             stroke-width="2"
                                             d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7h-1M8 7h-.688M13 5v4m-2-2h4" />
                                     </svg>
+                                    </a>
                                 </div>
+                                <!-- add to cart hover button end -->
+
+                                <!-- view product button start -->
+                                <div class="p-2 bg-white rounded-full ms-2">
+                                    <a href="detail">
+                                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                            viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-width="2"
+                                                d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
+                                            <path stroke="currentColor" stroke-width="2"
+                                                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        </svg>
+                                    </a>
+                                </div>
+                                <!-- view product button end -->
                             </div>
-                            <!-- add to cart hover button end-->
                         </div>
                         <div class="flex justify-between mt-4">
                             <div>
