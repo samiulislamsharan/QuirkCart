b8609f64545cf021b46af9859b84a12d119f89ab
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-25 00:46:57 +0600

    refactor(components): :recycle: optimize add to cart button in User Index.vue

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -21,13 +21,13 @@
                                 class="absolute inset-0 flex items-center justify-center opacity-0 cursor-pointer group-hover:opacity-100">
                                 <div class="p-2 bg-white rounded-full">
                                     <a @click="addToCart(product)">
-                                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-                                        viewBox="0 0 24 24">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="2"
-                                            d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7h-1M8 7h-.688M13 5v4m-2-2h4" />
-                                    </svg>
+                                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                            viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7h-1M8 7h-.688M13 5v4m-2-2h4" />
+                                        </svg>
                                     </a>
                                 </div>
                                 <!-- add to cart hover button end -->
@@ -51,10 +51,8 @@
                         <div class="flex justify-between mt-4">
                             <div>
                                 <h3 class="text-sm text-gray-700">
-                                    <a :href="product.href">
-                                        <span aria-hidden="true" class="absolute inset-0" />
-                                        {{ product.title }}
-                                    </a>
+                                    <span aria-hidden="true" class="inset-0" />
+                                    {{ product.title }}
                                 </h3>
                                 <p class="mt-1 text-sm text-gray-500">{{ product.brand.name }}</p>
                             </div>
