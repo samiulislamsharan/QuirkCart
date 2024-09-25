e68128835b188e5decf4f2a9f77ab564a747e332
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-26 01:51:47 +0600

    feat(resources): :sparkles: add CartResource class for transforming cart data

---
+++ app/Http/Resources/CartResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Helper\Cart;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CartResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        [$products, $cartItems] = $this->resource;
+
+        $total = $products->reduce(fn(?float $carry, Product $product) => $carry + $product->price * $cartItems[$product->id]['quantity']);
+
+        // $total = $products->sum(function (Product $product) use ($cartItems) {
+        //     return $product->price * $cartItems[$product->id]['quantity'];
+        // });
+
+        return [
+            'count' => Cart::getCount(),
+            'total' => $total,
+            'items' => $cartItems,
+            'products' => ProductResource::collection($products),
+        ];
+    }
+}
