50a1ee7543522c1e4c7619b8d61539fe6441464c
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-26 23:19:53 +0600

    refactor(models): :recycle: update UserAddress model to include fillable fields

--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class UserAddress extends Model
 {
     use HasFactory;
-}
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'phone',
+        'province',
+        'city',
+        'district',
+        'detail',
+        'is_main',
+    ];
