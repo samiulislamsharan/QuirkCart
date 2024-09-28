10ae4d39fd4b8ee421832ef5a3f1be0aeb0fe0d5
Samiul Islam <samiulislamsharan@gmail.com>
2024-09-28 17:05:20 +0600

    feat(components): :sparkles: add ProductListController.php

---
+++ app/Http/Controllers/User/ProductListController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ProductListController extends Controller
+{
+    public function index()
+    {
+        $products = Product::with('category', 'brand', 'product_images')->get();
+
+        return Inertia::render(
+            'User/ProductList',
+            [
+                'products' => $products
+            ]
+        );
+    }
+}
