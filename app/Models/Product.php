ff9dadc7f146d65d1c742e46fd4f0fc1fc2a75bb
Samiul Islam <samiulislamsharan@gmail.com>
2024-10-01 00:03:24 +0600

    feat(models): :sparkles: add scopeFiltered method to fitler query params from URL

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -52,5 +53,22 @@ class Product extends Model
     public function cart_items()
     {
         return $this->hasMany(CartItem::class);
+    }
+
+    public function scopeFiltered(Builder $query)
+    {
+        $query
+            ->when(request('brands'), function (Builder $q) {
+                $q->where('brand_id', request('brands'));
+            })
+            ->when(request('categories'), function (Builder $q) {
+                $q->where('category_id', request('categories'));
+            })
+            ->when(request('prices'), function (Builder $q) {
+                $q->whereBetween('price', [
+                    request('prices.from', 0),
+                    request('prices.to', 100000),
+                ]);
+            });
     }
 }
