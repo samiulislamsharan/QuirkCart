68b96f04396917a9dd8ddfc48cc6ff2834894334
Samiul Islam <samiulislamsharan@gmail.com>
2024-10-01 00:30:10 +0600

    refactor(controllers): :recycle: update ProductListController to filter products and paginate results

--- app/Http/Controllers/User/ProductListController.php
+++ app/Http/Controllers/User/ProductListController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -11,12 +12,13 @@ class ProductListController extends Controller
 {
     public function index()
     {
-        $products = Product::with('category', 'brand', 'product_images')->get();
+        $products = Product::with('category', 'brand', 'product_images');
+        $filterProducts = $products->filtered()->paginate(12)->withQueryString();
 
         return Inertia::render(
             'User/ProductList',
             [
-                'products' => $products
+                'products' => ProductResource::collection($filterProducts),
             ]
         );
     }
