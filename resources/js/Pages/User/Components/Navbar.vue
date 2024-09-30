50b7414fa9d57d20737a24fc6e12c555a452a12e
Samiul Islam <samiulislamsharan@gmail.com>
2024-10-01 01:06:50 +0600

    refactor(components): :recycle: update Navbar button element for logout link

--- resources/js/Pages/User/Components/Navbar.vue
+++ resources/js/Pages/User/Components/Navbar.vue
@@ -71,7 +71,7 @@
                                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
                         </li>
                         <li>
-                            <Link :href="route('logout')" method="post"
+                            <Link :href="route('logout')" method="post" as="button"
                                 class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
                             Sign out
                             </Link>
