d460da11ab9fbe374e0b3ddb1b66b4b2c363fcd2
Samiul Islam <samiulislamsharan@gmail.com>
2024-10-05 16:20:53 +0600

    feat(migrations): :sparkles: add migration file to add name and mobile number column to user_addresses table

---
+++ database/migrations/2024_10_03_165517_add_name_and_mobile_number_to_user_addresses_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_addresses', function (Blueprint $table) {
+            $table->string('name', 100)->after('id');
+            $table->string('mobile_number', 15)->after('country_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_addresses', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->dropColumn('mobile_number');
+        });
+    }
+};
