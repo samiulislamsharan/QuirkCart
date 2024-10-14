3034b501455259976a906dc414729ddd9cb9b15c
Samiul Islam <samiulislamsharan@gmail.com>
2024-10-15 03:14:04 +0600

    style(components): :pencil2: fix typo in add favorite button

--- resources/js/Pages/User/CartList.vue
+++ resources/js/Pages/User/CartList.vue
@@ -140,7 +140,7 @@
                                             </button>
                                             <div id="favourites-tooltip-1" role="tooltip"
                                                 class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                                                Add to favourites
+                                                Add to favorites
                                                 <div class="tooltip-arrow" data-popper-arrow></div>
                                             </div>
                                             <button type="button"
